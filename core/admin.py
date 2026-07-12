80a9052d39ec55478870037f5897d47b4c395f60
krishnapriya2401 <krishnapriyajan2006@gmail.com>
2026-07-12 11:23:29 +0530

    add role to jwt token payload

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Role Info', {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Role Info', {'fields': ('role',)}),
+    )
+
+admin.site.register(User, CustomUserAdmin)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import RoleTokenSerializer
+class RoleTokenView(TokenObtainPairView):
+    serializer_class = RoleTokenSerializer
 # Create your views here.
